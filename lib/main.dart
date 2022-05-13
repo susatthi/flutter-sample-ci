a69c9483b81fe2464512aeb9bf82dd5248a5905c
susa.keyber <susa@keyber.jp>
2022-05-14 08:08:12 +0900

    🚨 test: ワークフローが動くかテスト

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page 3'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page 4'),
     );
   }
 }
